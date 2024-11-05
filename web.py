6c97a2c8d0ffffab5e9f7bb2934e1e723536d011
Dobin Rutishauser <dobin.rutishauser@gmail.com>
2024-11-05 21:22:38 +0100

    initial version

---
+++ web.py
@@ -0,0 +1,61 @@
+from flask import Flask, render_template, jsonify, make_response
+
+import random
+import json
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/api/events', methods=['GET', 'POST'])
+def api_events():
+
+    # return json in data/notepad.json
+    with open('data/notepad.json') as f:
+        data = json.load(f)
+
+    response = make_response(jsonify(data))
+    return response
+
+@app.route('/api/detections', methods=['GET', 'POST'])
+def api_detections():
+    # Sample JSON data
+    data = [
+        "aaaa",
+        "bbbb",
+    ]
+    response = make_response(jsonify(data))
+    return response
+
+
+@app.route('/api/stats', methods=['GET', 'POST'])
+def api_stats():
+    #data = {
+    #    "events": random.randint(1, 100),
+    #    "users": random.randint(1, 100),
+    #}
+    #response = make_response(jsonify(data))
+    data = "Stat1: 1234 <br>  Stat2: 4444"
+    response = make_response(data)
+    return response
+
+
+@app.route('/api/reset', methods=['GET', 'POST'])
+def api_reset():
+    #data = {
+    #    "events": random.randint(1, 100),
+    #    "users": random.randint(1, 100),
+    #}
+    #response = make_response(jsonify(data))
+    data = "ok"
+    response = make_response(data)
+    return response
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
