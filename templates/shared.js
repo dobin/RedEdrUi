985a3246f367c75631e761f84cd416fd8c534623
Dobin Rutishauser <dobin.rutishauser@gmail.com>
2024-11-17 19:35:57 +0100

    more event log display improvements

--- templates/shared.js
+++ templates/shared.js
@@ -11,13 +11,14 @@
             let eventTitle = '';
             let eventHeader = '';
             let eventDetails = '';
+            let eventLong = '';
             let eventCallstack = '';
             for (const [key, value] of Object.entries(event)) {
                 // header
                 if (key === 'type' || key === 'time' || key === 'pid' || key === 'tid' || key === 'krn_pid') {
-                    eventHeader += `<span class="highlight_a">${key}:${value}</span>&nbsp`;
+                    eventHeader += `<span class="highlight_a">${key}:${value}</span> `;
                 } else if (key === 'func' || key === 'callback') {
-                    eventTitle += `<span class="highlight_b"><b>${value}</b></span>&nbsp;&nbsp;`;
+                    eventTitle += `<span class="highlight_b"><b>${value}</b></span> `;
 
                 // callstack
                 } else if (key == 'callstack') { 
@@ -25,19 +26,30 @@
 
                 // important
                 } else if (key === 'addr') {
-                    eventDetails += `<b>${key}:${value}</b>&nbsp;&nbsp;`;
+                    eventDetails += `<b>${key}:${value}</b> `;
                 } else if (key === 'protect') {
-                    eventDetails += `<b>${key}:${value}</b>&nbsp;&nbsp;`;
+                    eventDetails += `<b>${key}:${value}</b> `;
                 } else if (key === 'handle' && value != "FFFFFFFFFFFFFFFF") {
-                    eventDetails += `<b>${key}:${value}</b>&nbsp;&nbsp;`;
+                    eventDetails += `<b>${key}:${value}</b> `;
+
+                // long
+                } else if (key == 'name' || key == 'parent_name' || 
+                           key == 'image_path' || key == 'commandline' ||
+                           key == "working_dir") 
+                { 
+                    eventLong += `<span class="highlight_c">${key}:${value}</span> <br>`;
 
                 // rest
                 } else {
-                    eventDetails += `<span class="highlight_c">${key}:${value}</span>&nbsp;&nbsp;`;
+                    eventDetails += `<span class="highlight_c">${key}:${value}</span> `;
                 }
             }
 
-            eventDiv.innerHTML = eventTitle + eventHeader + "<br>" + eventDetails + "<br>" + eventCallstack;
+            eventDiv.innerHTML = eventTitle + eventHeader + "<br>" 
+                + eventDetails + "<br>" 
+                + eventLong
+                + eventCallstack;
+
             eventContainer.appendChild(eventDiv);
         });
     }
